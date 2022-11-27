b34fcacd1c5dbd393365bbd32923b2db74f55ae6
Santanu Bhattacharya <sxbhatta1@gmail.com>
2022-11-26 19:24:19 -0500

    init

--- app.js
+++ app.js
@@ -5,15 +5,49 @@ console.log(
 
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
+let a = 20;
+let b =4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let divide = a/b;
+console.log(add);
+console.log(minus);
+console.log(multiply);
+console.log(divide);
 
-// YOUR CODE HERE
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
 
-// YOUR CODE HERE
+console.log(num+str);
+console.log(num+str2);
+console.log(num + isPresent);
+console.log(isPresent + str);
+console.log(firstName + lastName);
+
+
+
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
 
-// YOUR CODE HERE
+console.log(val == str3);
+console.log(val ===str3);
+console.log(!isAwake);
+console.log("eleven" == str4 && val >= str3);
+console.log(!isAwake || isAwake);
+console.log(0 == false);
+console.log(0 ===false);
+console.log(0!= false);
+console.log(0!== false);
